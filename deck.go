0fca0ce71a0f5ae33a30d1470eac4e775846a3ce
NikhilSinghVIT <nikhilsingh.ns2014@gmail.com>
2020-05-16 23:41:04 +0530

    Fixed the randomise algorithm

--- deck.go
+++ deck.go
@@ -6,6 +6,7 @@ import (
 	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
 
 type deck []string
@@ -73,8 +74,10 @@ func newDeckFromFile(filename string) deck {
 //function to shuffle cards
 
 func (d deck) shuffle() {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
 	for i := range d {
-		newPosition := rand.Intn(len(d) - 1)
+		newPosition := r.Intn(len(d) - 1)
 		d[i], d[newPosition] = d[newPosition], d[i]
 	}
 }
